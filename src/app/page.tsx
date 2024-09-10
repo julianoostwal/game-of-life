d4e8ce2769a7911489da4c1f4768534b91284f06
Daniel <1175611@student.horizoncollege.nl>
2024-09-10 10:02:33 +0200

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 import Meteors from "@/components/magicui/meteors";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="bg-center bg-cover container mx-auto p-4  min-h-screen bg-gradient-to-r from-sky-700 to-sky-900">
-            <div className="animate-jump-in animate-delay-500">
+        <main className="bg-center bg-cover mx-auto p-4  min-h-screen bg-gradient-to-r from-sky-700 to-purple-900">
+            <div className="animate-jump-in animate-delay-500 ">
                 <Image
                     src={"spaceship.svg"}
                     width={300}
@@ -17,13 +18,16 @@ export default function Home() {
             <Meteors number={30} />
 
             <div className="flex items-center justify-center position: relative">
-                <div className="flex flex-col items-center animate-jump-in animate-delay-1000">
-                    <p className="text-5xl mb-8 protest-guerrilla-regular text-center">
+                <div className="flex flex-col items-center animate-jump-in animate-delay-[2000ms]
+">
+                    <p className="text-5xl mb-8 protest-guerrilla-regular text-center text-white animate-wiggle animate-thrice">
                         Game Of Life
                     </p>
-                    <Button variant={"destructive"} className="text-5xl p-12">
-                        Play!
-                    </Button>
+                    <Link href="/play">
+                        <Button variant={"destructive"} className="text-5xl p-12">
+                            Play!
+                        </Button>
+                    </Link>
                 </div>
             </div>
 
